4f30b71bb99e613d625fb357d62a071bd8fd4d6c
jack0 <jack@fooala.com>
2009-02-04 13:06:07 -0500

    Committed blog and sqlite db changes

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090126212101) do
+ActiveRecord::Schema.define(:version => 20090204175238) do
+
+  create_table "blogs", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "photos", :force => true do |t|
     t.integer  "parent_id"
