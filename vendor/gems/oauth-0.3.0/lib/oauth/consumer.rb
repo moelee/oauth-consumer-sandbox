82a471014f8905ae6404b592ed77bd94496bc622
jack0 <jack@fooala.com>
2009-04-14 20:03:43 -0400

    Access token now has scope

--- vendor/gems/oauth-0.3.0/lib/oauth/consumer.rb
+++ vendor/gems/oauth-0.3.0/lib/oauth/consumer.rb
@@ -119,7 +119,7 @@ module OAuth
       response=request(http_method,path,token,request_options,*arguments)
       if response.code=="200"
         temp = CGI.parse(response.body).inject({}){|h,(k,v)| h[k.to_sym]=v.join('&');h}
-	      temp[:"resource_names[]"] = temp[:"resource_names[]"].split('&') if temp[:"resource_names[]"]
+	      temp[:"resources[]"] = temp[:"resources[]"].split('&') if temp[:"resources[]"]
 	      temp[:"resource_urls[]"] = temp[:"resource_urls[]"].split('&') if temp[:"resource_urls[]"]
 	      return temp
       else 
