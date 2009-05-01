7f42a0c8e115d477e8b2bb658be3ca692435ceb0
Moses Lee <moses@fooala.com>
2009-05-01 02:17:38 -0400

    OpenID working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,24 @@
 # This controller handles the login/logout function of the site.  
 class SessionsController < ApplicationController
-
   # render new.rhtml
   def new
   end
 
   def create
+    if using_open_id?
+      open_id_authentication(params[:openid_url])
+    else
+      password_authentication(params[:name], params[:password])
+    end
+  end
+
+  def destroy
+    logout_killing_session!
+    flash[:notice] = "You have been logged out."
+    redirect_back_or_default('/')
+  end
+protected
+  def password_authentication(name, password)
     logout_keeping_session!
     user = User.authenticate(params[:login], params[:password])
     if user
@@ -16,8 +29,7 @@ class SessionsController < ApplicationController
       self.current_user = user
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
-      #redirect_back_or_default('/')
-      redirect_to dashboard_path
+      successful_login
       flash[:notice] = "Logged in successfully"
     else
       note_failed_signin
@@ -27,10 +39,38 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destroy
-    logout_killing_session!
-    flash[:notice] = "You have been logged out."
-    redirect_back_or_default('/')
+  def open_id_authentication(openid_url)
+    authenticate_with_open_id(openid_url, :required => [:nickname, :email]) do |result, identity_url, registration|
+      if result.successful?
+        @user = User.find_or_initialize_by_identity_url(identity_url)
+        if @user.new_record?
+          @user.login = registration['nickname']
+          @user.email = registration['email']
+          @user.save(false)
+        end
+        self.current_user = @user
+        successful_login
+      else
+        failed_login result.message
+      end
+    end
+  end
+
+
+private
+  def successful_login
+    session[:user_id] = @current_user.id
+    if session[:oauth_token]
+      redirect_to authorize_path(:oauth_token => session[:oauth_token])
+      session[:oauth_token] = nil
+    else
+      redirect_to dashboard_path
+    end
+  end
+
+  def failed_login(message)
+    flash[:error] = message
+    redirect_to(new_session_url)
   end
 
 protected
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090501060944) do
+ActiveRecord::Schema.define(:version => 20090501061526) do
 
   create_table "access_tokens", :force => true do |t|
     t.string   "token"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(:version => 20090501060944) do
     t.datetime "updated_at"
     t.string   "remember_token",            :limit => 40
     t.datetime "remember_token_expires_at"
+    t.string   "identity_url"
   end
 
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
